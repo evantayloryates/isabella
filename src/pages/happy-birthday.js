8bbadbe6906f0af10c0f5253405a81343f2da0a6
Evan Taylor Yates <evantayloryates@gmail.com>
2023-06-30 12:19:39 -0700

    add Happy Birthday player

--- src/pages/happy-birthday.js
+++ src/pages/happy-birthday.js
@@ -5,7 +5,14 @@ const HappyBirthday = () => {
   return (
     <>
       <div className="center">
-        <h2>Happy Birthday</h2>
+        <iframe 
+          src="https://player.vimeo.com/video/533224004?h=723717c1a6" 
+          width="960" 
+          height="540" 
+          frameborder="0" 
+          allow="autoplay; fullscreen; picture-in-picture" 
+          allowfullscreen 
+        />
       </div>
     </>
   )
